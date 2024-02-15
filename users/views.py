ac1d86ca162ac2cfe77679c1b8c1741d0ee78c06
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-15 19:08:11 +0300

    add UserRegisterAPIView

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, status
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 
-from users.models import Payment
+from users.models import Payment, User
 from users.serializers.payment import PaymentSerializer
+from users.serializers.users import UserSerializer
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,18 @@ class PaymentViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('course', 'lesson', 'method')
     ordering_fields = ('date',)
+
+
+class UserRegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create(
+                email=request.data['email'],
+            )
+            user.set_password(request.data['password'])
+            user.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
