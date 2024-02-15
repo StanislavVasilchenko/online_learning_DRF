1e2a173f78f481c334c939b3e870895121d9a6dd
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-15 21:59:50 +0300

    add permissions

--- materials/views/lessons.py
+++ materials/views/lessons.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Lesson
+from materials.permissions import IsModerator, IsStaff
 from materials.serializers.lessons import LessonSerializer
 
 
@@ -12,18 +14,22 @@ class LessonsListAPIView(generics.ListAPIView):
 class LessonCreateAPIView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsStaff]
 
 
 class LessonDetailAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated | IsModerator]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator]
 
 
 class LessonDeleteAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsStaff]
