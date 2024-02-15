4a87a5ed56d43b0d9697674bba86ec4caf8625dd
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-15 19:07:36 +0300

    add TokenObtain

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet
+from users.views import PaymentViewSet, UserRegisterAPIView
 
 app_name = UsersConfig.name
 
@@ -10,5 +15,7 @@ router.register(r'payment', PaymentViewSet, basename='payment')
 
 
 urlpatterns = [
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserRegisterAPIView.as_view(), name='register_user'),
 ] + router.urls
