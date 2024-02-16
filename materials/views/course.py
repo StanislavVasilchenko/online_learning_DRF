00b6b14fc0449624daf805645c92277d408afd20
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-17 00:12:58 +0300

    add get_queryset

--- materials/views/course.py
+++ materials/views/course.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course
-from materials.permissions import IsModerator, IsUserIsOwner, IsStaff
+from materials.permissions import IsModerator, IsUserIsOwner
 from materials.serializers.course import CourseSerializer
 
 
@@ -11,13 +11,17 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
 
     def get_permissions(self):
-        if self.action in ['retrieve', 'update', 'partial_update']:
-            self.permission_classes = [IsAuthenticated, IsModerator | IsUserIsOwner]
+        if self.action in ['create', 'retrieve_delete']:
+            self.permission_classes = [IsAuthenticated, IsUserIsOwner]
         else:
-            self.permission_classes = [IsAuthenticated, IsStaff]
+            self.permission_classes = [IsAuthenticated, IsUserIsOwner | IsModerator]
         return [permission() for permission in self.permission_classes]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
+    def get_queryset(self):
+        if self.queryset.filter(user=self.request.user).exists():
+            return Course.objects.filter(user=self.request.user)
+        elif self.request.user.is_superuser or self.request.user.groups.filter(name='moderator').exists():
+            return self.queryset
