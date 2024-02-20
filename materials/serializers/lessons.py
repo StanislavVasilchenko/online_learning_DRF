a2bca17a9e0aa46344f06c28066c0de9121fc3f7
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-20 22:33:12 +0300

    add in LessonSerializer LessonValidator

--- materials/serializers/lessons.py
+++ materials/serializers/lessons.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 
 from materials.models import Lesson
+from materials.validators import LessonValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [LessonValidator(
+            field='video_url'
+        )
+        ]
 
 
 class LessonsSerializerForUser(serializers.ModelSerializer):
@@ -14,3 +19,4 @@ class LessonsSerializerForUser(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         exclude = ['id', 'user']
+
