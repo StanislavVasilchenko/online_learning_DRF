77161266bb6d87c9998ea0c4880ad018f7923055
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-24 15:31:03 +0300

    add swagger_auto_schema

--- materials/views/subscription.py
+++ materials/views/subscription.py
@@ -1,11 +1,37 @@
+from drf_yasg import openapi
+from drf_yasg.openapi import TYPE_INTEGER, TYPE_STRING
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
 from materials.models import Subscription, Course
 
 
 class SubscriptionAPIView(APIView):
+
+    @swagger_auto_schema(operation_description="Test",
+                         request_body=openapi.Schema(
+                             type=openapi.TYPE_OBJECT,
+                             properties={
+                                 'course_id': openapi.Schema(
+                                     type=TYPE_INTEGER,
+                                     description="Id of the course")
+                             },
+                             responses={
+                                 204: openapi.Response(
+                                     description="OK",
+                                     schema=openapi.Schema(
+                                         type=openapi.TYPE_OBJECT,
+                                         properties={
+                                             'message': openapi.Schema(
+                                                 type=TYPE_STRING,
+                                                 description="Subscription delete/Subscription created")
+                                         }
+                                     )
+                                 )
+                             }
+                         ))
     def post(self, *args, **kwargs):
         user = self.request.user
         course_id = self.request.data.get("course")
