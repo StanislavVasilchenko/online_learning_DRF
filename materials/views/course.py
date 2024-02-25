1ecfa28da7e58c8a3ffb4a711f6ac601f7262fc9
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-25 19:55:37 +0300

    remove CoursePaymentAPIView

--- materials/views/course.py
+++ materials/views/course.py
@@ -1,15 +1,11 @@
-from rest_framework import viewsets, status
-from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets
+
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from materials.models import Course
 from materials.paginators import LessonsAndCoursePaginator
 from materials.permissions import IsModerator, IsUserIsOwner
 from materials.serializers.course import CourseSerializer
-from materials.services import get_url_payment
-from users.models import Payment
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -32,23 +28,3 @@ class CourseViewSet(viewsets.ModelViewSet):
             return Course.objects.filter(user=self.request.user)
         elif self.request.user.is_superuser or self.request.user.groups.filter(name='moderator').exists():
             return self.queryset
-
-
-class CoursePaymentAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, *args, **kwargs):
-        user = self.request.user
-        course_id = self.request.data.get("course")
-
-        item = get_object_or_404(Course, pk=course_id)
-        if item:
-            payment_url = get_url_payment(item)
-
-            payment = Payment.objects.create(user=user,
-                                             course=item,
-                                             amount=item.price
-                                             )
-            payment.save()
-            return Response({"url": payment_url})
-        return Response({"error": "Payment does not exist."}, status=status.HTTP_400_BAD_REQUEST)
