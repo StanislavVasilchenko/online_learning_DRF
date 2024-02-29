353cbfd247181608e49396f5ab957dbd99ff3569
stanislav <stanislav.vasilchenko@yandex.ru>
2024-02-29 20:39:00 +0300

    .env

--- private_settings.py
+++ private_settings.py
@@ -10,3 +10,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+
+BROKER_URL = os.getenv("BROKER_URL")
+RESULT_BACKEND = os.getenv("RESULT_BACKEND")
